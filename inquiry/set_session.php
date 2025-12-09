80f49251681be5b152766118dc23f89b676d77d2
W-ain <it222293@trident.ac.jp>
2025-12-09 12:23:18 +0900

    Update set_session.php

--- inquiry/set_session.php
+++ inquiry/set_session.php
@@ -4,11 +4,12 @@ session_start();
 if (isset($_GET['id'])) {
     $_SESSION['view_debt_id'] = $_GET['id'];
     // 修正箇所: debt_changeフォルダ内のrecord.phpへ遷移
-    header("Location: /debtapp/debt_change/record.php");
+    header("Location: /debt_change/record.php");
     exit;
 } else {
     // そのまま（直下のinquiry.phpへ）
     header("Location: inquiry.php");
     exit;
 }
+
 ?>
