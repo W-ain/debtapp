07e3cf1ee993d3894fd3dbd7db7e82cda7c6f696
W-ain <it222293@trident.ac.jp>
2025-12-09 12:02:42 +0900

    Update config.php

--- config.php
+++ config.php
@@ -76,12 +76,13 @@ define('MAIL_ENCRYPTION', 'tls');
 // ================================
 define('GOOGLE_CLIENT_ID', '887906658821-1spgtqg6mu506eslavhjpbntc3hb9bar.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://localhost/debtapp/login/google_callback.php');
-
+// define('GOOGLE_REDIRECT_URI', 'http://localhost/debtapp/login/google_callback.php');
+define('GOOGLE_REDIRECT_URI', 'https://debtapp-565547399529.asia-northeast1.run.app/login/google_callback.php');
 // Google API 認証URL
 $google_auth_endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
 $google_token_endpoint = "https://oauth2.googleapis.com/token";
 $google_userinfo_endpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
 
-?>
+
+
 
