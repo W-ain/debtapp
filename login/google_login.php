00cd6a60e3295b54715775160a57ea3ffcba081d
W-ain <it222293@trident.ac.jp>
2025-12-10 10:06:44 +0900

    Update google_login.php

--- login/google_login.php
+++ login/google_login.php
@@ -4,8 +4,7 @@ require_once '../config.php';
 $google_auth_url = "https://accounts.google.com/o/oauth2/v2/auth?" . http_build_query([
     'client_id'     => '887906658821-1spgtqg6mu506eslavhjpbntc3hb9bar.apps.googleusercontent.com',
     // 'redirect_uri'  => 'http://localhost/debtapp/login/google_callback.php',
-    'redirect_uri'  => 'https://debtapp-565547399529.asia-northeast1.run.app
-/login/google_callback.php',
+    'redirect_uri'  => 'https://debtapp-565547399529.asia-northeast1.run.app/login/google_callback.php',
     'response_type' => 'code',
     'scope'         => 'email profile',
     'access_type'   => 'offline',
@@ -15,4 +14,5 @@ $google_auth_url = "https://accounts.google.com/o/oauth2/v2/auth?" . http_build_
 // Googleログインページへリダイレクト
 header("Location: $google_auth_url");
 exit;
+
 
