c49975ac48ef46035f6ecf2dfe37b21379ba8e3e
akahane2525 <it222002@trident.ac.jp>
2025-12-19 12:29:41 +0900

    Update save_debt.php

--- Regist/save_debt.php
+++ Regist/save_debt.php
@@ -359,6 +359,7 @@ $title    = $is_error ? '処理失敗' : '処理完了';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>処理結果</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="icon" href="../favicon.ico?v=1">
     <style>
         body {
             margin: 0;
@@ -449,6 +450,7 @@ function redirectToHome() {
 <?php
 exit;
 ?>
+
 
 
 
